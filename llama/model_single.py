fix: mixed and full precision

llama/model_single.py
@@ -224,9 +224,10 @@ class Transformer(nn.Module):
             )
             mask = torch.triu(mask, diagonal=start_pos + 1).type_as(h)
 
-
+        embeddings = []
         for layer in self.layers:
             h = layer(h, start_pos, freqs_cis, mask)
+            embeddings.append(h)
         h = self.norm(h)
         output = self.output(h[:, :, :])  # only compute last logits
-        return output.float()
+        return output.float(),embeddings